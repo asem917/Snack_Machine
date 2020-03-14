3411a32d540d842acb053e76606806116518f74b
Magic Systems <asem@gmail.com>
2020-03-14 23:11:36 +0200

    second commit

--- src/main/java/com/progressoft/induction/Snack.java
+++ src/main/java/com/progressoft/induction/Snack.java
@@ -5,17 +5,14 @@ public class Snack {
     public static final Money PRICE_OF_CHEWING_GUM = Money.HALF_DINAR;
     public static final Money PRICE_OF_CHIPS = Money.QUARTER_DINAR;
     public static final Money PRICE_OF_CHOCOLATE = Money.DINAR;
-    private SnackType snackType;
+
 
 
     public Snack(SnackType snackType) {
-        this.snackType = snackType;
+
 
 
     }
 
-    public SnackType getSnackType() {
-        return snackType;
-    }
 
 }
--- src/main/java/com/progressoft/induction/SnackMachine.java
+++ src/main/java/com/progressoft/induction/SnackMachine.java
@@ -43,7 +43,6 @@ public class SnackMachine {
     }
 
     public Money moneyInTransaction() {
-
         return moneyInTransaction;
     }
 
